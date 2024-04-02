908dc2c9d522f0ee7cb007103e1cf81f36171d7d
SrBauer <bauer.jonathan.jj@gmail.com>
2024-04-02 17:08:39 -0300

    first commit

---
+++ cardapio/src/main/java/com/example/cardapio/controlles/FoodController.java
@@ -0,0 +1,35 @@
+package com.example.cardapio.controlles;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+import com.example.cardapio.food.FoodResquestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodResquestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+
+}
